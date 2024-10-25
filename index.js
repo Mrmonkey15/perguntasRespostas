d06854d8a8c2d50000f3f60fe09f35b06f5fab35
Mrmonkey15 <lucas.network15@gmail.com>
2024-10-25 17:29:48 -0300

    upday

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 //estrutura básica do express para iniciar um servidor 
 
+const { urlencoded } = require('body-parser');
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 4000;
 // Informando ao Express que está usando o ejs
 app.set('view engine', 'ejs');
+// Informando pasta de arquivos estáticos
+app.use(express.static('public'));
+//bodyParser 
+app.use(urlencoded({extended:false}))
 
+
+
+
+//rotas
 app.get("/", (req, res) => {
-  res.send("Teste de rota1");
+  res.render("index")
 });
+//criou a rota da pagina perguntar
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar")
+});
+// criou rota para salvar o formulário
+app.post("/salvarpergunta", (req, res)=>{
+  let description = req.body.descricao
+  res.send(`formulário recebido ${description}`)
+})
 
 app.listen(port);
